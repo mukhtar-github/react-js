74f52409252bb0b7f4650c34f918af52921816da
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-28 16:55:06 +0100

    Update files

--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react';
 
 function Greeting() {
   return <h4>This is Mukhtar and this is my first component</h4>;
 }
+
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'))
