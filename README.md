41a15ee7adade21733b6cd9939da6b590728f21e
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-29 14:38:33 +0100

    Update file

--- README.md
+++ README.md
@@ -228,3 +228,40 @@ const Author = () => (
 
 ReactDom.render(<BookList />, document.getElementById("root"));
 ```
+
+## JSX Javascript
+
+```javascript
+import React from "react";
+import ReactDom from "react-dom";
+
+// CSS
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg"
+        alt=""
+      />
+      <h1>I Love You to the Moon and Back</h1>
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        Amelia Hepworth
+      </h4>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
+```
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -8,11 +8,6 @@ function BookList() {
   return (
     <section className='booklist'>
     <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
     </section>
   );
 }
@@ -20,18 +15,11 @@ function BookList() {
 const Book = () => {
   return (
   <article className='book'>
-    <Image />
-    <Title />
-    <Author />
-    </article>
+    <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg" alt="" />
+    <h1>I Love You to the Moon and Back</h1>
+    <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }} >Amelia Hepworth</h4>
+  </article>
   )
 };
-
-const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg" alt="" />
-};
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }} >Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
