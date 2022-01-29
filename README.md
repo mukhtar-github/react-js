eafed6d464085b02b7d49569c9431200f10ecdb2
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-29 11:03:25 +0100

    Update files

--- README.md
+++ README.md
@@ -100,8 +100,26 @@ function BookList() {
 }
 
 const Book = () => {
-  return <article>this is a book</article>;
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
 };
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
 ```
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -6,15 +6,24 @@ import ReactDom from "react-dom";
 function BookList() {
   return <section>
     <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
   </section>;
-}
+};
 
 const Book = () => {
-  return <article>this is a car</article>
-}
+  return (
+  <article>
+    <Image></Image>
+    <Title />
+    <Author />
+    </article>
+  )
+};
+
+const Image = () => {
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg" alt="" />
+};
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+const Author = () => <h4>Amelia Hepworth</h4>
 
 ReactDom.render(<BookList />, document.getElementById("root"));
