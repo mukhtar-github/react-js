f789d52017505594d402be74dee503cf2cbdb4df
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-29 08:26:33 +0100

    Update file

--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// stateless functional component
+// always return JSX
+
 function Greeting() {
   return <h4>This is Mukhtar and this is my first component</h4>;
 }
