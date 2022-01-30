b84fc5ab2c78fbbc2d996ac480bd709f42796fa5
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-30 17:11:19 +0100

    Update file

--- README.md
+++ README.md
@@ -538,3 +538,41 @@ const Book = (props) => {
   );
 };
 ```
+
+## Key Prop And Spread Operator
+
+React would want to keep track of changes, ranging from adding, removing, and updating items in our list.
+
+So, we use key as a keyword to assign unique values to our list, as long as they're changing, but some times index is being used, if one is sure that the items in array are not goig to change.
+
+```javascript
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth"
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen and Sandie Sonke'
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/71gGPRRlyTL._AC_UY218_.jpg',
+    title: 'The Vanishing Half: A Novel',
+    author: 'Brit Bennett, Shayna Small, et al'
+  }
+];
+
+function BookList() {
+  return (
+    <section className="booklist">{books.map((book) => {
+      return <Book key={ book.id } book={book}/>
+    })}</section>
+  );
+}
+...
+```
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -4,26 +4,29 @@ import './index.css';
 
 const books = [
   {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth"
-},
-{
-  img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
-  title: 'Our Class is a Family',
-  author: 'Shannon Olsen and Sandie Sonke'
-},
-{
-  img: 'https://m.media-amazon.com/images/I/71gGPRRlyTL._AC_UY218_.jpg',
-  title: 'The Vanishing Half: A Novel',
-  author: 'Brit Bennett, Shayna Small, et al'
-}
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth"
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen and Sandie Sonke'
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/71gGPRRlyTL._AC_UY218_.jpg',
+    title: 'The Vanishing Half: A Novel',
+    author: 'Brit Bennett, Shayna Small, et al'
+  }
 ];
 
 function BookList() {
   return (
     <section className="booklist">{books.map((book) => {
-      return <Book book={book}/>
+      return <Book key={ book.id } book={book}/>
     })}</section>
   );
 }
