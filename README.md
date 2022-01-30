825d7ff29d5a44dde4eff2640530d32298a97de3
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-30 10:33:24 +0100

    Update files

--- README.md
+++ README.md
@@ -318,4 +318,46 @@ const Book = (props) => {
 };
 
 ReactDom.render(<BookList />, document.getElementById("root"));
+
+// Props
+// Setup Vars
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
+  title: "Our Class is a Family",
+  author: "Shannon Olsen and Sandie Sonke",
+};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  // console.log(props);
+  return (
+    <article className="book">
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
 ```
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -4,15 +4,23 @@ import './index.css';
 
 // Props
 // Setup Vars
-const title = "I Love You to the Moon and Back";
-const author = "Amelia Hepworth";
-const img = "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg";
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth"
+};
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+  title: 'Our Class is a Family',
+  author: 'Shannon Olsen and Sandie Sonke'
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book job='developer' />
-      <Book title='random title' number={22} />
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
     </section>
   );
 }
@@ -21,12 +29,9 @@ const Book = (props) => {
   // console.log(props);
   return (
     <article className="book">
-      <img src= {img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <p>{props.job}</p>
-      <p>{props.title}</p>
-      <p>{props.number}</p>
+      <img src= {props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
