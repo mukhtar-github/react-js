4f14172b0ac0f674c43352a12b912e4982d62863
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-30 15:29:05 +0100

    Update files

--- README.md
+++ README.md
@@ -411,7 +411,6 @@ function BookList() {
 }
 
 const Book = ({ img, title, author, children }) => {
-  // const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
@@ -422,4 +421,34 @@ const Book = ({ img, title, author, children }) => {
     </article>
   );
 };
+// or
+const Book = (props) => {
+  const { img, title, author, children } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
+    </article>
+  );
+};
+```
+
+## Simple List
+
+```javascript
+const names = ["Musa", "Isa", "Abba"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
+
+function BookList() {
+  return <section className="booklist">{newNames}</section>;
+}
+// UI Output
+Musa;
+Isa;
+Abba;
 ```
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -2,40 +2,37 @@ import React from "react";
 import ReactDom from "react-dom";
 import './index.css';
 
-// Props
-// Setup Vars
-const firstBook = {
+const books = [
+  {
   img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
   title: "I Love You to the Moon and Back",
   author: "Amelia Hepworth"
-};
-
-const secondBook = {
+},
+{
   img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
   title: 'Our Class is a Family',
   author: 'Shannon Olsen and Sandie Sonke'
-};
+}
+];
+const names = ['Musa', 'Isa', 'Abba'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
+console.log(newNames);
 
 function BookList() {
   return (
-    <section className="booklist">
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} >
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque eos quas accusantium iusto ex sint voluptatem? Repellendus tenetur ex reiciendis ullam nam asperiores recusandae, facilis voluptatum accusamus cum ipsam molestias?</p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
-    </section>
+    <section className="booklist">{newNames}</section>
   );
 }
 
 const Book = (props) => {
   const { img, title, author } = props;
-  console.log(props)
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
