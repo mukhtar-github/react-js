e44dee43b11e7a050c1782a35f36d33a77c204b8
Mukhtar <mukhtartanimugarba@gmail.com>
2022-01-30 10:44:34 +0100

    Update files

--- README.md
+++ README.md
@@ -361,3 +361,18 @@ const Book = (props) => {
   );
 };
 ```
+
+## Props Destructuring
+
+```javascript
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+```
--- react-js/src/index.js
+++ react-js/src/index.js
@@ -26,12 +26,12 @@ function BookList() {
 }
 
 const Book = (props) => {
-  // console.log(props);
+  const { img, title, author } = props
   return (
     <article className="book">
-      <img src= {props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
